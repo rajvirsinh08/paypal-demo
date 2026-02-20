5d556c2aa6c330ca1af97cf8bc16568b302e08c7
Rajvirsinh Dabhi <rajvirsinhdabhi1@gmail.com>
2026-02-20 13:59:03 +0530

    fhfhrgfrf

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,3 +1,27 @@
+// import app from "../server";
+// import mongoose from "mongoose";
+
+// let cached = (global as any).mongoose;
+
+// if (!cached) {
+//   cached = (global as any).mongoose = { conn: null, promise: null };
+// }
+
+// async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(process.env.MONGODB_URI!);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// export default async function handler(req: any, res: any) {
+//   await connectDB();
+//   return app(req, res);
+// }
 import app from "../server";
 import mongoose from "mongoose";
 
@@ -11,7 +35,11 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI!);
+    mongoose.set("bufferCommands", false); // 🔥 VERY IMPORTANT
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI!, {
+      serverSelectionTimeoutMS: 5000,
+    });
   }
 
   cached.conn = await cached.promise;
